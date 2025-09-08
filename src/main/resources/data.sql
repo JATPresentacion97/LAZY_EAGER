239a394562aa0c9f079ecaa7836c6133137b9007
John Albert T. Presentacion <johnalbertpresentacion@gmail.com>
2025-09-08 20:48:23 +0800

    git init

--- src/main/resources/data.sql
+++
@@ -1,8 +0,0 @@
-insert into author(id, name) values (1, 'Ada Lovelace');
-insert into author(id, name) values (2, 'Alan Turing');
-insert into author(id, name) values (3, 'Grace Hopper');
-
-insert into book(id, title, author_id) values (1, 'Notes on the Analytical Engine', 1);
-insert into book(id, title, author_id) values (2, 'Computing Machinery and Intelligence', 2);
-insert into book(id, title, author_id) values (3, 'A Manual for the Operations...', 3);
-insert into book(id, title, author_id) values (4, 'COBOL Design Principles', 3);
---
+++ src/main/resources/import.sql
@@ -0,0 +1,8 @@
+insert into author (id, name) values (1, 'Ada Lovelace');
+insert into author (id, name) values (2, 'Alan Turing');
+insert into author (id, name) values (3, 'Grace Hopper');
+
+insert into book (id, title, author_id) values (1, 'Notes on the Analytical Engine', 1);
+insert into book (id, title, author_id) values (2, 'Computing Machinery and Intelligence', 2);
+insert into book (id, title, author_id) values (3, 'A Manual for the Operations of the Automatic Sequence Controlled Calculator', 3);
+insert into book (id, title, author_id) values (4, 'COBOL Design Principles', 3);
