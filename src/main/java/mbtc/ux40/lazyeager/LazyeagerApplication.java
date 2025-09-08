2930a9fe11a1097d2c0a65ef79c5da4fe4a153e8
John Albert T. Presentacion <johnalbertpresentacion@gmail.com>
2025-09-08 20:27:11 +0800

    git init

---
+++ src/main/java/mbtc/ux40/lazyeager/LazyeagerApplication.java
@@ -0,0 +1,13 @@
+package mbtc.ux40.lazyeager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LazyeagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LazyeagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mbtc/ux40/lazyeager/LazyeagerApplicationTests.java
@@ -0,0 +1,13 @@
+package mbtc.ux40.lazyeager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LazyeagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
